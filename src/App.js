2a3c784590b610bd168bd7dfa9a657c56d4417f2
faisalsaysin <faisalsaysin@gmail.com>
2020-02-01 16:04:32 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import logo from './logo.svg';
+import Menu from './components/MenuComponent';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <NavbarBrand href="./">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
