4133fcd5b78a19a2e2854d531daeb1e195163c09
faisalsaysin <faisalsaysin@gmail.com>
2020-02-08 16:49:42 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -16,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
